38ea7763d3776186d192bfbcb3d1bbf862acf3a0
fahmi54321 <fahmiabdulaziz44@gmail.com>
2021-05-01 13:51:01 +0700

    search bar

--- app/src/main/java/com/android/a5searchbar/MainActivity.java
+++ app/src/main/java/com/android/a5searchbar/MainActivity.java
@@ -1,14 +1,107 @@
 package com.android.a5searchbar;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.miguelcatalan.materialsearchview.MaterialSearchView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    MaterialSearchView searchView;
+    ListView listView;
+
+    String[] listSource = {
+            "Fahmi",
+            "Abdul",
+            "Aziz",
+            "Muhammad",
+            "Ega",
+            "Dermawan",
+            "One",
+            "Two",
+            "Three",
+            "Four",
+            "Five",
+            "Six",
+            "Seven",
+            "Eight",
+            "Nine",
+            "Ten",
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Material Search");
+        toolbar.setTitleTextColor(Color.parseColor("#FFFFFF"));
+
+        listView = (ListView) findViewById(R.id.listView);
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1,listSource);
+        listView.setAdapter(adapter);
+
+        searchView = (MaterialSearchView)findViewById(R.id.search_view);
+        searchView.setOnSearchViewListener(new MaterialSearchView.SearchViewListener() {
+            @Override
+            public void onSearchViewShown() {
+
+            }
+
+            @Override
+            public void onSearchViewClosed() {
+
+                //if closed Search view, listView will return default
+                ArrayAdapter adapter = new ArrayAdapter(MainActivity.this, android.R.layout.simple_list_item_1,listSource);
+                listView.setAdapter(adapter);
+
+            }
+        });
+        searchView.setOnQueryTextListener(new MaterialSearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                if (newText !=null && !newText.isEmpty()){
+                    List<String> listFound = new ArrayList<>();
+                    for (String item:listSource){
+                        if (item.contains(newText)){
+                            listFound.add(item);
+                        }
+                    }
+
+                    ArrayAdapter adapter = new ArrayAdapter(MainActivity.this, android.R.layout.simple_list_item_1,listFound);
+                    listView.setAdapter(adapter);
+                }else{
+                    ArrayAdapter adapter = new ArrayAdapter(MainActivity.this, android.R.layout.simple_list_item_1,listSource);
+                    listView.setAdapter(adapter);
+                }
+                return true;
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu,menu);
+        MenuItem item = menu.findItem(R.id.search);
+        searchView.setMenuItem(item);
+
+        return true;
     }
 }
